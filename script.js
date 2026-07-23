e661d9f224853b8f2b4958d58c49f2b3e1dc5e19
BobAggiustaTutto <martina.sciout@gmail.com>
2026-07-23 14:45:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -182,8 +182,8 @@ function moveEnemy() {
         // ANGOLI RUOTATI DI 90° PER ALLINEARE LA TESTA
         if (nextX > enemy.x) enemyAngle = Math.PI * 0.5;       // Va a Destra
         else if (nextX < enemy.x) enemyAngle = Math.PI * 1.5;  // Va a Sinistra
-        else if (nextY > enemy.y) enemyAngle = Math.PI;         // Va in Basso
-        else if (nextY < enemy.y) enemyAngle = 0;       // Va in Alto
+      else if (nextY > enemy.y) enemyAngle = Math.PI * 1.0;  // Va in Basso
+        else if (nextY < enemy.y) enemyAngle = Math.PI * 2.0;  // Va in Alto (o 0)
 
         enemy.x = nextX;
         enemy.y = nextY;
